6f93380537a9674f24a7905161d179338e3e9c96
Đoàn Việt Khang <109060462+vietkhang06@users.noreply.github.com>
2026-05-25 17:31:35 +0700

    feat(admin): implement admin management module

---
+++ app/src/main/java/com/example/cinemabooking/ui/admin/room/AdminSeatTemplatePlaceholder.java
@@ -0,0 +1,15 @@
+package com.example.cinemabooking.ui.admin.room;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cinemabooking.R;
+
+public class AdminSeatTemplatePlaceholder extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_seat_template);
+    }
+}
