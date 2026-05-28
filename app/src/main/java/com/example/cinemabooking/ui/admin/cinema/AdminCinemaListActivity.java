a387ba67f2d3dcf87e0b5690470de2367a7af72d
DavidHHH2404 <khanghuynh3467@gmail.com>
2026-05-28 15:30:19 +0700

    fix: merge conflicts

--- app/src/main/java/com/example/cinemabooking/ui/admin/cinema/AdminCinemaListActivity.java
+++ app/src/main/java/com/example/cinemabooking/ui/admin/cinema/AdminCinemaListActivity.java
@@ -80,8 +80,8 @@ public class AdminCinemaListActivity extends BaseActivity {
 
             @Override
             public void onViewDetailsClick(Cinema cinema) {
-                Intent intent = new Intent(AdminCinemaListActivity.this, com.example.cinemabooking.ui.admin.cinema.AdminCinemaDetailActivity.class);
-                intent.putExtra(com.example.cinemabooking.ui.admin.cinema.AdminCinemaDetailActivity.EXTRA_CINEMA_ID, cinema.cinemaId);
+                Intent intent = new Intent(AdminCinemaListActivity.this, AdminCinemaDetailActivity.class);
+                intent.putExtra(AdminCinemaDetailActivity.EXTRA_CINEMA_ID, cinema.cinemaId);
                 startActivity(intent);
             }
         });
@@ -89,7 +89,7 @@ public class AdminCinemaListActivity extends BaseActivity {
         rv.setAdapter(adapter);
 
         btnAdd.setOnClickListener(v ->
-                startActivity(new Intent(this, com.example.cinemabooking.ui.admin.cinema.AdminCinemaFormActivity.class))
+                startActivity(new Intent(this, AdminCinemaFormActivity.class))
         );
 
         android.widget.EditText etSearch = findViewById(R.id.etSearchCinema);
